dc18f63ccbbd15db09deb28991b6accd7212287d
Dan Spencer <dspence180@gmail.com>
2021-07-08 14:14:26 -0500

    Added password description option; display full save path

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const {
 const createPassword = require('./utils/createPassword');
 const savePasswords = require('./utils/savePassword');
 
-program.version('1.1.0').description('Simple password generator');
+program.version('1.1.1').description('Simple password generator');
 
 // Command options
 program
@@ -22,12 +22,13 @@ program
   .option('-u, --use-symbols <symbols>', 'use only specified symbols', pwSymbols)
   .option('-o, --use-only <characters>', 'use only specified chars', '')
   .option('-c, --count <number>', 'number of passwords to generate', defCount)
+  .option('-d, --description <string>', 'description of password', '')
   .option('-s, --save', 'save password to file', false)
   .option('-f, --file-name <filename>', 'name of password save file', defFilename)
   .option('-d, --debug', 'display debug info', false)
   .parse();
 
-const { count, save, fileName, debug } = program.opts();
+const { count, description, save, fileName, debug } = program.opts();
 
 if (debug) {
   console.table(program.opts());
@@ -41,11 +42,10 @@ for (let i = 0; i < count; i++) {
   //Output generated password
   log(chalk.green('Generated Password: ') + chalk.bold(generatedPasswords[i]));
 }
+if (description) log(chalk.green('Description: ') + description);
 
 // Save to file
-if (save) {
-  savePasswords(generatedPasswords, fileName);
-}
+if (save) savePasswords(generatedPasswords, fileName, description);
 
 // Copy to clipboard (if only 1 password)
 if (count === 1) {
--- utils/savePassword.js
+++ utils/savePassword.js
@@ -3,13 +3,17 @@ const path = require('path');
 const os = require('os');
 const chalk = require('chalk');
 
-const savePasswords = (passwords, fileName, mode='a') => {
-  fs.open(path.join(__dirname, '../', fileName), mode, 666, (e, fd) => {
+const savePasswords = (passwords, fileName, description) => {
+  const savePath = path.join(__dirname, '../', fileName);
+
+  fs.open(savePath, 'a', 666, (e, fd) => {
     passwords.forEach((password, i) => {
-      fs.write(fd, password + os.EOL, null, 'utf-8', () => {
+      const writeOutput = password + (description ? ` // ${description}` : '') + os.EOL;
+      
+      fs.write(fd, writeOutput, null, 'utf-8', () => {
         if (i === passwords.length - 1) {
           fs.close(fd); // close when last password is written to file
-          console.log(chalk.yellow(`Password(s) saved to "${fileName}"`));
+          console.log(chalk.yellow(`Password(s) saved to "${savePath}"`));
         }
       });
     });
